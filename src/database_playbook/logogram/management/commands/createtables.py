46344d5762b18da71013fe09a948792d4084a868
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-02-06 17:07:31 +0300

    refactor(flashcards): refactor management commands
    - refactor management commands to create and drop tables
    
    [ Fixes #163700576]

--- src/database_playbook/logogram/management/commands/createtables.py
+++ src/database_playbook/logogram/management/commands/createtables.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from logogram.users.create_table.create_table import create_user_table
+from logogram.flashcards.create_table.create_table import (
+    create_flashcards_table)
 
 
 class Command(BaseCommand):
@@ -8,6 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             create_user_table()
+            create_flashcards_table()
         except Exception as error:
             raise CommandError(error)
         self.stdout.write(self.style.SUCCESS('Database Tables Created'))
--- src/database_playbook/logogram/management/commands/droptables.py
+++ src/database_playbook/logogram/management/commands/droptables.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from logogram.users.drop_table.drop_table import drop_user_table
+from logogram.flashcards.drop_table.drop_table import drop_flashcards_table
 
 
 class Command(BaseCommand):
@@ -7,6 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            drop_flashcards_table()
             drop_user_table()
         except Exception as error:
             raise CommandError(error)
